7368297199d80c6e9bae74c5e02f3f073fe9867c
Strahinja Ajvaz <19544513+strahinjaajvaz@users.noreply.github.com>
2022-02-16 21:02:28 +1100

    chore: added accordion component

---
+++ src/components/Accordion/Accordion.test.tsx
@@ -0,0 +1,40 @@
+import { Accordion } from "./Accordion";
+import {
+  renderWithTheme,
+  screen,
+  fireEvent,
+  waitFor,
+} from "../../test/utils/render";
+
+describe("Accordian Component", () => {
+  it("should reader the Accordion component", () => {
+    const content = "content";
+    renderWithTheme(
+      <Accordion title="Music">
+        <p>content</p>
+      </Accordion>
+    );
+
+    const btnEl = screen.getByRole("button");
+    expect(btnEl).toBeInTheDocument();
+
+    const textEl = screen.queryAllByText(content);
+    expect(textEl).toHaveLength(0);
+  });
+
+  it("should toggle the content when you click the Accordion title", async () => {
+    const content = "content";
+    renderWithTheme(
+      <Accordion title="Music">
+        <p>content</p>
+      </Accordion>
+    );
+
+    const btnEl = screen.getByRole("button");
+    fireEvent.click(btnEl);
+
+    await waitFor(() => screen.findByText(content));
+
+    expect(screen.getByText(content)).toBeInTheDocument();
+  });
+});
---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+interface Props {
+  title: string;
+  children: React.ReactNode;
+}
+
+export function Accordion({ title, children }: Props) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleToggleList = () => setIsOpen((prevState) => !prevState);
+
+  return (
+    <StyledContainer>
+      <StyledButton onClick={handleToggleList}>{title}</StyledButton>
+      {isOpen && children}
+    </StyledContainer>
+  );
+}
+
+const StyledContainer = styled.div`
+  border-radius: ${(props) => props.theme.borderRadius};
+  background-color: ${(props) => props.theme.colors.listBackgroundColor};
+`;
+
+const StyledButton = styled.button`
+  padding: 0.5rem 0;
+  width: 100%;
+  text-align: center;
+  line-height: 1.5rem;
+  font-size: 0.875rem;
+  cursor: pointer;
+  background-color: ${(props) => props.theme.colors.backgroundColor};
+  color: ${(props) => props.theme.colors.color};
+  border-radius: ${(props) => props.theme.borderRadius};
+  border: 2px solid ${(props) => props.theme.colors.backgroundColor};
+
+  &:hover {
+    background-color: ${(props) => props.theme.colors.color};
+    color: ${(props) => props.theme.colors.backgroundColor};
+  }
+`;
